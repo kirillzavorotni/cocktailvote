30a2f6b577eb6f355d873b9ac9f204c97f90a100
kirillzavorotni <kirillzavorotni@gmail.com>
2020-01-20 23:29:29 +0300

    feat: add js template

--- public/index.php
+++ public/index.php
@@ -1,1 +1,35 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>coctailvotes</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body
+    <div class="wrapper">
+        <div class="item">
+            <div class="item_header">Item 1</div>
+            <div class="item_content">
+                <img src="https://i.picsum.photos/id/1/300/300.jpg">
+            </div>
+            <div class="item_footer">
+                <button class="item_footer_button">Voite</button>
+            </div>
+        </div>
+        <div class="item">
+            <div class="item_header">Item 1</div>
+            <div class="item_content">
+                <img src="https://i.picsum.photos/id/1008/300/300.jpg">
+            </div>
+            <div class="item_footer">
+                <button class="item_footer_button" id="vote_btn">Voite</button>
+            </div>
+        </div>
+    </div>
+
+    <script src="./js/script.js"></script>
+</body>
+</html>
